023f8be97637856ad88a865f4276d0bdb4cd688a
Kiho Park <152009026+KihoPark811@users.noreply.github.com>
2025-02-21 16:08:13 +0900

    Title: 세 정수형 변수 선언, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250221/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 7;
+    int b = 23;
+    int c = 30;
+
+    cout << a << " + " << b << " = " << c;
+
+    return 0;
+}
