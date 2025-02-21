2013ea98ca1348741c177733e1e3c84058b768cf
Kiho Park <152009026+KihoPark811@users.noreply.github.com>
2025-02-21 15:59:40 +0900

    Title: 정수 선언하고 뺄셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250221/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 97, b = 13;
+    int c = a - b;
+    cout << a << " - " << b << " = " << c;
+
+    return 0;
+}
