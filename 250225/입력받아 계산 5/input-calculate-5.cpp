8f67355b2f51639c33cd97f9133534491a6beea0
Kiho Park <152009026+KihoPark811@users.noreply.github.com>
2025-02-25 12:13:00 +0900

    Title: 입력받아 계산 5, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250225/입력받아 계산 5/input-calculate-5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    // cin >> 변수1 >> 변수2; : 공백을 기준으로 갯수에 맞게 입력할 수 있다.
+    cin >> a >> b;
+    cout << a + b;
+    return 0;
+}
